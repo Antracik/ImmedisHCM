a1e5307ca6759a4bb46ea1df4055a298587d46f1
Preslav Panaiotov <preslav.miroslavov@gmail.com>
2020-11-03 10:40:36 +0200

    Add project files.

---
+++ ImmedisHCM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ImmedisHCM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
